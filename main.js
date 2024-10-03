5f93d0c7bd2854616dadc6ebe071a02685a92844
Kah2804 <kalliani.lima@escola.pr.gov.br>
2024-10-03 12:25:02 -0300

    dadad

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RF','RO','RR','SC','SP','SE','TO'];
-let i=0
-while(i<estados.length){
-    console.log(i);
-    console.log(estados[i]);
-    i++;
-}
-for (let j = 0; j < estados.length; j++) {
-    const element = estados[j];
-    console.log(j);
-    console.log(estados[j]);
-    
-}
+// let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RF','RO','RR','SC','SP','SE','TO'];
+// let i=0
+// while(i<estados.length){
+//     console.log(i);
+//     console.log(estados[i]);
+//     i++;
+// }
+// for (let j = 0; j < estados.length; j++) {
+//     const element = estados[j];
+//     console.log(j);
+//     console.log(estados[j]);
+// }
