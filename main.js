7a280ab0f7379efb73f7d0f9a79fd3dd8d3eb7d6
Kah2804 <kalliani.lima@escola.pr.gov.br>
2024-10-03 11:49:03 -0300

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RF','RO','RR','SC','SP','SE','TO'];
+let i=0
+while(i<27){
+    console.log(i);
+    i=i+1;
+}
