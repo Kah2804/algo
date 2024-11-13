d81055bf6b685cf23033f279e93b04a4d3bd827b
Kah2804 <kalliani.lima@escola.pr.gov.br>
2024-11-13 08:12:21 -0300

    main.js

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-// let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RF','RO','RR','SC','SP','SE','TO'];
-// let i=0
-// while(i<estados.length){
-//     console.log(i);
-//     console.log(estados[i]);
-//     i++;
-// }
-// for (let j = 0; j < estados.length; j++) {
-//     const element = estados[j];
-//     console.log(j);
-//     console.log(estados[j]);
-// }
-let idade= 22;
-if(idade>23){
-    console.log('dirige ae')
+function criaCartao (categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML =  `
+    <div class="conteudo-cartao">
+    <h3>${categoria}</h3>
+    <div class="pergunta-cartao">
+        <p>${pergunta}</p>
+    </div>
+    <div class="resposta-cartao">
+        <p>${resposta}</p>
+    </div>
+</div>
+    `
+    let respostaV = false;
+    function virar(){
+        respostaV = !respostaV;
+        cartao.classList.toggle('active', respostaV);
+    }
+    cartao.addEventListener('click', virar)
+    container.appendChild(cartao);
 }
- else{
-    console.log('vai preso')
-
- }
